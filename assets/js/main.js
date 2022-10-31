2552a5e6ce7a8dbb3a0d2baf29f61fabbf1006a7
Tiago <ottiagos@gmail.com>
2022-10-31 20:47:54 -0300

    toggle diametro js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,19 @@
-console.log("aaa11aaba");                   
+const toggleDiametro = () => {
+  const selectDiametro = document.querySelector(".select-diametro");
+  const selectLargura = document.querySelector(".select-largura");
+  const selectComprimento = document.querySelector(".select-comprimento");
+
+  if (selectDiametro && selectDiametro.style.display === "none") {
+    selectDiametro.style.display = "block";
+    selectLargura.style.display = "none";
+    selectComprimento.style.display = "none";
+  } else {
+    selectDiametro.style.display = "none";
+    selectLargura.style.display = "block";
+    selectComprimento.style.display = "block";
+  }
+};
+
+document.getElementById("checkboxRedondas").addEventListener("change", () => {
+  toggleDiametro();
+});
